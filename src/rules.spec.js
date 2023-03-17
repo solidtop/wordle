706fff08a9693c8bfcd8239c66c09db80e522092
Axel Asp <solidtop.asp@gmail.com>
2023-03-17 10:40:03 +0100

    Update tests with spread operator

--- src/rules.spec.js
+++ src/rules.spec.js
@@ -18,23 +18,23 @@ describe('checkGuess()', () => {
         const expected = ['C', 'Y', 'K', 'L', 'A'];
         const response = checkGuess('CYKLA', mockWord);
         response.forEach((obj, i) => {
-            expect(obj).toEqual({letter: expected[i]});
+            expect(obj).toEqual({...obj, letter: expected[i]});
         });
     });
 
     it('Should return all results with "correct"', () => {
         const response = checkGuess('CYKLA', mockWord);
         response.forEach(obj => {
-            expect(obj).toEqual({result: 'correct'});
+            expect(obj).toEqual({...obj, result: 'correct'});
         });
     });
 
     it('Should return first result with "incorrect"', () => {
-        expect(checkGuess('HUKLA', mockWord)[0]).toEqual({result: 'incorrect'});
+        expect(checkGuess('HUKLA', mockWord)[0]).toEqual({...obj, result: 'incorrect'});
     });
 
     it('Should return first result with "misplaced"', () => {
-        expect(checkGuess('LYKLA')[0]).toEqual({result: 'misplaced'});
+        expect(checkGuess('LYKLA')[0]).toEqual({...obj, result: 'misplaced'});
     });
 
     it('Should return results with "incorrect", "misplaced", "incorrect", "correct", "incorrect"', () => {
@@ -42,7 +42,7 @@ describe('checkGuess()', () => {
         
         const response = checkGuess('HALLÅ', mockWord);
         response.forEach((obj, i) => {
-            expect(obj).toEqual({result: expected[i]});
+            expect(obj).toEqual({...obj, result: expected[i]});
         });
     });
 });
