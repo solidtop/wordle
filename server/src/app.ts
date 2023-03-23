492148f862faa4a387c79d3275a6dc6ae9e492a8
Axel Asp <solidtop.asp@gmail.com>
2023-03-23 15:43:20 +0100

    Setup express and routes

---
+++ server/src/app.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import { engine } from 'express-handlebars';
+import bodyParser from 'body-parser';
+import router from './routers';
+
+export default function initApp() {
+    const app = express();
+
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(express.json());
+    app.engine('handlebars', engine());
+    app.set('view engine', 'handlebars');
+
+    app.use('/', express.static('public'));
+    app.use(router);
+
+    return app;
+}
