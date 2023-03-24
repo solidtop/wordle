65a5a77cbbc935a041aafab1ad304541f87f6cee
Axel Asp <solidtop.asp@gmail.com>
2023-03-24 12:35:32 +0100

    Add apiAdapter

---
+++ server/src/utils/ApiAdapter.ts
@@ -0,0 +1,16 @@
+import fetch from 'node-fetch';
+
+const API_URL: string = 'https://raw.githubusercontent.com/dwyl/english-words/master/words_dictionary.json';
+
+class APIAdapter {
+
+    async fetchWords() {
+        const res = await fetch(API_URL);
+        const data = await res.json();
+        return data;
+    }
+
+}
+
+export default APIAdapter;
+
