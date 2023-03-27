e586a705d158e48219e96daaf30b527c9a2a91f1
Axel Asp <solidtop.asp@gmail.com>
2023-03-27 20:37:32 +0200

    Move gameState logic to seperate function

--- server/src/controllers/gameController.ts
+++ server/src/controllers/gameController.ts
@@ -1,5 +1,53 @@
+import { Result } from './checkGuess';
+
 export function getElapsedTime(dateStr: string): number {
     const startDate = new Date(dateStr);
     const endDate = new Date();
     return (endDate.getTime() - startDate.getTime());
 }
+
+interface GameState {
+    gameIsFinished: boolean;
+    results: Result[] | undefined;
+    numGuesses: number;
+    playerHasWon?: boolean;
+    message?: string;
+    secretWord?: string;
+    score?: number;
+    gameDuration?: number;
+}
+
+interface StateData {
+    results: Result[] | undefined;
+    secretWord: string;
+    isExactMatch: boolean | undefined;
+    gameStartTimestamp: string;
+    numGuesses: number;
+}
+
+export function getGameState(stateData: StateData): GameState {
+    const { results, secretWord, isExactMatch, gameStartTimestamp, numGuesses } = stateData;
+    const playerHasWon = isExactMatch;
+    const gameIsFinished = playerHasWon || numGuesses <= 1;
+    const updatedNumGuesses = gameIsFinished && playerHasWon ? numGuesses : numGuesses - 1;
+   
+    if (gameIsFinished) {
+        const gameDuration = getElapsedTime(gameStartTimestamp);
+        const message = playerHasWon ? 'Congratulations, you have won!' : 'You lose! Booo';
+        return {
+            gameIsFinished,
+            playerHasWon,
+            message,
+            results,
+            numGuesses: updatedNumGuesses,
+            secretWord,
+            gameDuration,
+        };
+    } 
+
+    return {
+        gameIsFinished,
+        results,
+        numGuesses: updatedNumGuesses,
+    }
+} 
--- server/src/routes/api/guess.ts
+++ server/src/routes/api/guess.ts
@@ -1,19 +1,8 @@
 import express from 'express';
 import checkGuess, { Result } from '../../controllers/checkGuess';
-import { getElapsedTime } from '../../controllers/gameController';
+import { getGameState } from '../../controllers/gameController';
 
 const router = express.Router();
-
-interface GameResult {
-    gameIsFinished: boolean;
-    results: Result[] | undefined;
-    numGuesses: number;
-    playerHasWon?: boolean;
-    message?: string;
-    secretWord?: string;
-    score?: number;
-    gameDuration?: number;
-}
 
 router.post('/guess', (req, res) => {
     const { secretWord, gameStartTimestamp = '', numGuesses = 0 } = req.session;
@@ -21,44 +10,27 @@ router.post('/guess', (req, res) => {
         res.status(500).send('Secret word is not defined');
         return;
     }
-
-    const guess = req.body.guess;
-    const {isValid, isExactMatch, error, results} = checkGuess(guess, secretWord);
+    const guess:string = req.body.guess;
+    const { isValid, isExactMatch, error, results } = checkGuess(guess, secretWord);
     if (!isValid) {
         res.status(400).json({ error });
         return;
     }
 
-    const gameDuration = getElapsedTime(gameStartTimestamp);
-    const playerHasWon = isExactMatch;
-    const gameIsFinished = playerHasWon || numGuesses <= 1;
-    const updatedNumGuesses = gameIsFinished && playerHasWon ? numGuesses : numGuesses - 1;
-    req.session.numGuesses = updatedNumGuesses;
-   
-    let gameResult: GameResult;
-    if (gameIsFinished) {
-        const message = playerHasWon ? 'Congratulations, you have won!' : 'You lose! Booo';
-        gameResult = {
-            gameIsFinished,
-            playerHasWon,
-            message,
-            results,
-            numGuesses: updatedNumGuesses,
-            secretWord,
-            score: 0, //NOTE: Not yet implemented
-            gameDuration,
-        };
-
+    const gameState = getGameState({
+        results, 
+        secretWord, 
+        isExactMatch, 
+        gameStartTimestamp, 
+        numGuesses,
+    });
+    req.session.numGuesses = gameState.numGuesses;
+    
+    if (gameState.gameIsFinished) {
         req.session.destroy(err => { if (err) throw err }); //Clear the session when game finishes
-    } else {
-        gameResult = {
-            gameIsFinished,
-            results,
-            numGuesses: updatedNumGuesses,
-        }
     }
    
-    res.json({ ...gameResult });
+    res.json({ ...gameState });
 });
 
 export default router;
