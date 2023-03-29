c74f15c0d9639cc5cc18510d9b99180b45dbd207
Axel Asp <solidtop.asp@gmail.com>
2023-03-29 22:09:51 +0200

    change file name and add more styling

--- client/src/components/GuessForm.jsx
+++ client/src/components/GuessForm.jsx
@@ -4,17 +4,17 @@ export default function GuessForm({ onGuess }) {
     const [text, setText] = useState('');
 
     return (
-        <form onSubmit={e => {
+        <form class="guess-form" onSubmit={e => {
             e.preventDefault();
             e.target.reset();
             onGuess(text);
         }}>
             <input 
                 type="text" 
-                id='guessInput'
+                id="guessInput"
                 onChange={e => setText(e.target.value)}
             />
-            <button type="submit">ENTER</button>
+            <button type="submit" className="btn primary">ENTER</button>
         </form>
     );
 }
