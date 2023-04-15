75ceedb13883f5c1d081db8503cd3ba2c43f5355
Axel Asp <solidtop.asp@gmail.com>
2023-04-15 12:04:23 +0200

    include dist files

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import initApp from './src/app';
 const app = initApp();
 
-const PORT: number = 3080;
+const PORT: number = 5080;
 app.listen(PORT, (): void => {
     console.log(`Listening on port ${PORT}`);
 });
